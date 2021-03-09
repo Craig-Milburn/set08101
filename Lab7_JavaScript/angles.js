27da3d70b42a6ac42817b5feb1753e03738cde74
Craig Milburn <craig.milburn.1994@gmail.com>
2021-03-09 21:18:04 +0000

    First week of Java Script

---
+++ Lab7_JavaScript/angles.js
@@ -0,0 +1,32 @@
+function myAngles() {
+	var angle = "No value";
+	var x = document.getElementById("size").value;
+	var b = parseInt(x)
+	if (b > 90) {
+		if (b == 180) {
+			angle = "Straight Angle";
+		}
+		else if (b > 360) {
+			angle = "Value too great";
+		}
+		else if (b > 180) {
+			angle = "Reflex Angle";
+		}
+		else {
+			angle = "Obtuse Angle";
+		}
+	}
+	else if (b < 90) {
+		if (b > 0) {
+		angle = "Acute Angle";
+		}
+		else {
+			angle = "No angle";
+		}
+	}
+	else {
+		angle = "Right Angle";
+	}
+	 document.getElementById("result").innerHTML = angle;
+}
+	
---
+++ Lab7_JavaScript/hello.js
@@ -0,0 +1,7 @@
+function helloFunction(){
+	alert('Hello Napier');
+}
+function myFunction() {
+	document.getElementById("demo").innerHTML = "Hello";}
+//function angles(){//
+	
