cec0e34eebfa490678988ef4d0802612472f5455
Craig Milburn <craig.milburn.1994@gmail.com>
2021-03-29 22:15:44 +0100

    correcting small elements of previous commit

--- Amnesia_Palace/game2.js
+++ Amnesia_Palace/game2.js
@@ -1,22 +1,39 @@
-function mGame() {
-	var x = document.getElementById("rem").value;
+document.addEventListener("DOMContentLoaded", function(event) { 
+    
+
 	var i = 1;
-	var items = ["picture", "violin", "umbrella", "marble", 
-				"kettle", "teddy bear", "apple", "suitcase",
-				"crib"];
-	if (b == 180) {
-		angle = "Straight Angle";
+	var notList = [];
+
+	function mGame() {
+		document.getElementById("rem")
+		.addEventListener("keyup", function(event) {
+		event.preventDefault();
+		if (event.keyCode === 13) {
+			document.getElementById("mButton").click();
+		}
+	});
+		var x = document.getElementById("rem").value;
+		var items = ["picture", "violin", "umbrella", "marble", 
+					"kettle", "teddy bear", "apple", "suitcase",
+					"crib","Picture", "Violin", "Umbrella", "Marble", 
+					"Kettle", "Teddy bear", "Apple", "Suitcase",
+					"Crib", "Teddy Bear"];
+		if (items.includes(x)) {
+			if (notList.includes(x)) {
+				box = i.toString();
+				document.getElementById(box).innerHTML = "Wrong";
+			}
+			else {
+				box =  i.toString();
+				i += 1;
+				notList.push(x);
+				document.getElementById(box).innerHTML = x;
+			}
+		}
+		else {
+			box = i.toString();
+			document.getElementById(box).innerHTML = "Wrong";
+		}
+		document.getElementById("rem").value = "";
 	}
-	else if (b > 360) {
-		angle = "Value too great";
-	}
-	else if (b == 360) {
-		angle = "Circle";
-	}
-	else if (b > 180) {
-		angle = "Reflex Angle";
-	}
-	else {
-		angle = "Obtuse Angle";
-	}
-}
+});
