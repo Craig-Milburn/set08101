53692ea24e374ac7913fb314e8a6ba237ec99f2a
Craig Milburn <craig.milburn.1994@gmail.com>
2021-03-29 21:41:54 +0100

    adding level of error checking and simplifying form submission

--- Amnesia_Palace/game.js
+++ Amnesia_Palace/game.js
@@ -1,16 +1,34 @@
 	var i = 1;
+	var notList = [];
 function mGame() {
+	document.getElementById("rem")
+		.addEventListener("keyup", function(event) {
+		event.preventDefault();
+		if (event.keyCode === 13) {
+			document.getElementById("mButton").click();
+		}
+	});
 	var x = document.getElementById("rem").value;
 	var items = ["picture", "violin", "umbrella", "marble", 
 				"kettle", "teddy bear", "apple", "suitcase",
-				"crib"];
+				"crib","Picture", "Violin", "Umbrella", "Marble", 
+				"Kettle", "Teddy bear", "Apple", "Suitcase",
+				"Crib", "Teddy Bear"];
 	if (items.includes(x)) {
-		box =  i.toString();
-		i += 1;
-		document.getElementById(box).innerHTML = x;
+		if (notList.includes(x)) {
+			box = i.toString();
+			document.getElementById(box).innerHTML = "Wrong";
+		}
+		else {
+			box =  i.toString();
+			i += 1;
+			notList.push(x);
+			document.getElementById(box).innerHTML = x;
+		}
 	}
 	else {
 		box = i.toString();
 		document.getElementById(box).innerHTML = "Wrong";
 	}
+	document.getElementById("rem").value = "";
 }
