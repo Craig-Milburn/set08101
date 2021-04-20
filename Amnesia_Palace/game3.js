7de5162eed7eb947f2b0648e6f426bf8b0ba3406
Craig Milburn <craig.milburn.1994@gmail.com>
2021-04-20 18:25:41 +0100

    Images added and media resize formed

--- Amnesia_Palace/game3.js
+++ Amnesia_Palace/game3.js
@@ -4,24 +4,30 @@ var notList = [];
 function mGame() {
 	var x = document.getElementById("rem").value;
 	x = x.toLowerCase();
-	console.log(x);
 	var items = ["picture", "violin", "umbrella", "marble", 
 				"kettle", "teddy bear", "apple", "suitcase",
 				"crib"];
 	if (!items.includes(x) || notList.includes(x)) {
 		box = i.toString();
 		document.getElementById(box).innerHTML = "Wrong";
-		console.log(x);
 	}
 	else {
 		box =  i.toString();
 		i += 1;
 		notList.push(x);
 		document.getElementById(box).innerHTML = x;
-		console.log(notList);
 	}
 	if (i == 10) {
 		document.getElementById("wd").innerHTML = "Congratulations on remembering all of the items!";
+		var button = document.createElement("button");
+		button.innerHTML = "Go To Home";
+		
+		var body = document.getElementsByClassName("story_")[0];
+		body.appendChild(button);
+
+		button.addEventListener ("click", function() {
+		window.location.href='../index.html';
+	});
 	}
 	document.getElementById("rem").value = "";
 }
