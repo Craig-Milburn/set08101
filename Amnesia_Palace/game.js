8b0cb64a87598e32fb9d8c9432894e8796e10731
Craig Milburn <craig.milburn.1994@gmail.com>
2021-04-21 21:36:56 +0100

    updating .js file name and correcting it in html

--- Amnesia_Palace/game.js
+++ Amnesia_Palace/game.js
@@ -1,27 +1,33 @@
-	var i = 1;
-	var notList = [];
+var i = 1;
+var notList = [];
+
 function mGame() {
 	var x = document.getElementById("rem").value;
+	x = x.toLowerCase();
 	var items = ["picture", "violin", "umbrella", "marble", 
 				"kettle", "teddy bear", "apple", "suitcase",
-				"crib","Picture", "Violin", "Umbrella", "Marble", 
-				"Kettle", "Teddy bear", "Apple", "Suitcase",
-				"Crib", "Teddy Bear"];
-	if (items.includes(x)) {
-		if (notList.includes(x)) {
-			box = i.toString();
-			document.getElementById(box).innerHTML = "Wrong";
-		}
-		else {
-			box =  i.toString();
-			i += 1;
-			notList.push(x);
-			document.getElementById(box).innerHTML = x;
-		}
-	}
-	else {
+				"crib"];
+	if (!items.includes(x) || notList.includes(x)) {
 		box = i.toString();
 		document.getElementById(box).innerHTML = "Wrong";
+	}
+	else {
+		box =  i.toString();
+		i += 1;
+		notList.push(x);
+		document.getElementById(box).innerHTML = x;
+	}
+	if (i == 10) {
+		document.getElementById("wd").innerHTML = "Congratulations on remembering all of the items!";
+		var button = document.createElement("button");
+		button.innerHTML = "Go To Home";
+		
+		var body = document.getElementsByClassName("story_")[0];
+		body.appendChild(button);
+
+		button.addEventListener ("click", function() {
+		window.location.href='../index.html';
+	});
 	}
 	document.getElementById("rem").value = "";
 }
